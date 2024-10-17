0357adc04465c239b8d18a4f22850e3ff9423ce3
VIETMAP\truongpn <truongpn@vietmap.vn>
2024-10-17 15:09:45 +0700

    feat:create stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,29 +13,38 @@ namespace api.Controllers
     [ApiController]
     public class StockController : ControllerBase
     {
-    private readonly ApplicationDBContext _context;
-    public StockController(ApplicationDBContext context)
-    {
-        _context = context;
-    }
-
-    [HttpGet]
-    public IActionResult GetAll()
-    {
-      var stocks = _context.Stocks.Select(s => s.ToStockDto()).ToList();
-      return Ok(stocks);
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id)
-    {
-      var stock = _context.Stocks.Find(id);
-
-      if(stock == null)
+      private readonly ApplicationDBContext _context;
+      public StockController(ApplicationDBContext context)
       {
-        return NotFound();
+          _context = context;
       }
-      return Ok(stock.ToStockDto());
-    }
+
+      [HttpGet]
+      public IActionResult GetAll()
+      {
+        var stocks = _context.Stocks.Select(s => s.ToStockDto()).ToList();
+        return Ok(stocks);
+      }
+
+      [HttpGet("{id}")]
+      public IActionResult GetById([FromRoute] int id)
+      {
+        var stock = _context.Stocks.Find(id);
+
+        if(stock == null)
+        {
+          return NotFound();
+        }
+        return Ok(stock.ToStockDto());
+      }
+
+      [HttpPost]
+      public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+      {
+        var stockModel = stockDto.ToStockFromCreateDTO();
+        _context.Stocks.Add(stockModel);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+      }
     }
 }
