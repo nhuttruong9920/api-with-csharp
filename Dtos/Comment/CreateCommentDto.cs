f9bfe27b6538fda99cefb5fa50e2910295082d8b
VIETMAP\truongpn <truongpn@vietmap.vn>
2024-10-22 16:42:38 +0700

    feat: filtering sorting and pagination

--- Dtos/Comment/CreateCommentDto.cs
+++ Dtos/Comment/CreateCommentDto.cs
@@ -13,8 +13,8 @@ namespace api.Dtos.Comment
       [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
       public string Title { get; set; } = string.Empty;
       [Required]
-      [MinLength(5, ErrorMessage = "Content must be 5 characters")]
-      [MaxLength(280, ErrorMessage = "Content cannot be over 280 characters")]
+      [MinLength(5, ErrorMessage = "Title must be 5 characters")]
+      [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
       public string Content { get; set; } = string.Empty;
       public int StockId { get; set; }
     }
